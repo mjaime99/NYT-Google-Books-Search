e24f81bf3d15830492a98ecc35d1cef7df4a6a40
unknown <mike_jaime@live.com>
2019-01-12 11:48:35 -0700

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Serve up static assets if in production (running on Heroku)
-app.use(express.static(__dirname + "/client/build"));
+app.use(express.static(__dirname + "/client/public"));
 
 // enable CORS, use:
 // https://enable-cors.org/server_expressjs.html
